642da751195b2711b5019dc39c507f9747b63d71
Kry765 <krzysztof.kleka@poczta.onet.pl>
2023-03-21 22:32:41 +0100

    correct navigation

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import '../scss/Home.scss'
+import { Link } from 'react-router-dom'
 
 export default class Home extends React.Component {
 	render() {
@@ -7,11 +8,16 @@ export default class Home extends React.Component {
 			<div>
 				<nav>
 					<div className='nav'>
+						<div className='nav__menu--logo'></div>
 						<div className='nav__burgericon'></div>
-						{/* <div></div> */}
-						<div className='nav--menu'>home</div>
-						<div className='nav--menu'>about me</div>
-						<div className='nav--menu'>contact</div>
+						<div className='nav__menu'>About me</div>
+						<div className='nav__menu'>Contact</div>
+						<div className='nav__menu'>
+							<Link to='Login'>Log in</Link>
+						</div>
+						<div className='nav__menu--register-btn'>
+							<Link to='/Register'>Sing up</Link>
+						</div>
 					</div>
 				</nav>
 				<header>
@@ -21,7 +27,7 @@ export default class Home extends React.Component {
 							<p className='header__welcome-text'>
 								Gain valuable knowledge of relational database management systems that will help you find a job in IT
 							</p>
-							<button className='header__welcome-btn'>get started</button>
+							<button className='header__welcome-btn'>Get Started</button>
 						</div>
 					</div>
 				</header>
